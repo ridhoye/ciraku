85b5d73bf868228c134c31dad985f32a930ee613
Wisnunot <wisnuseptahariantoputra13@gmail.com>
2025-09-20 19:02:40 +0700

    ubah

---
+++ db.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document123</title>
+</head>
+<body>
+    <h1>sssss</h1>
+</body>
+</html>
