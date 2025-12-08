1cce72cdbefa706329e5641af646f64b45a11092
Ridhoye <ridhoye1922@gmail.com>
2025-12-08 11:03:46 +0700

    pwebaikan

--- dasbord/home.php
+++ dasbord/home.php
@@ -160,29 +160,6 @@
   <!-- JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script>
-    
-    // Inisialisasi AOS
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-out-cubic',
-      once: true
-    });
-
-    // Navbar blur effect saat scroll
-    window.addEventListener('scroll', function() {
-      const navbar = document.querySelector('.navbar');
-      if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
-  </script>
-
-    <!-- JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script>
     // Inisialisasi AOS
